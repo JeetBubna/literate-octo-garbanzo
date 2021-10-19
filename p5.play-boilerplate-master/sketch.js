c978e99cfb72f2278f73a48e0be2979905ec73e3
JeetBubna <91751318+JeetBubna@users.noreply.github.com>
2021-10-19 16:57:51 +0530

    Add files via upload

---
+++ p5.play-boilerplate-master/sketch.js
@@ -0,0 +1,74 @@
+var hr, mn , sc;
+var hrAngle , mnAngle , scAngle;
+var bcgr;
+
+function setup() {
+  
+    createCanvas(800,400);
+     angleMode(DEGREES);
+     
+}
+
+function draw() {
+  background(0,0,0);
+  translate(200,200);
+  
+  text("The time is" + hr + " - " + min + " - " + sc + ".", 0, 100);
+
+  hr = hour();
+  mn = minute();
+  sc = second();
+
+  scAngle = map(sc,0,60,0,360);
+  mnAngle = map(mn,0,60,0,360);
+  hrAngle = map(hr % 12,0,12,0,360);
+
+
+  //creating the sc hand
+  push();
+  rotate(scAngle);
+  stroke(255,0,0);
+  strokeWeight(7);
+  line(0,0,100,0);
+  pop()
+
+  //Creating the min hand
+  push();
+  rotate(mnAngle);
+  stroke(0,255,0);
+  strokeWeight(7);
+  line(0,0,75,0);
+  pop()
+
+  //Creating the hour hand
+  push();
+  rotate(hrAngle);
+  stroke(0,0,255);
+  strokeWeight(7);
+  line(0,0,50,0);
+  pop()
+
+  
+  //The arcs
+
+  stroke(255,0,255);
+  point(0,0)
+
+  strokeWeight(10);
+  noFill();
+
+  stroke(255,0,0);
+  arc(0,0,300,300,0,scAngle);
+
+  stroke(0,255,0);
+  arc(0,0,280,280,0,mnAngle);
+
+  stroke(0,0,255);
+  arc(0,0,260,260,0,hrAngle);
+
+//  fill(255, 255, 255);
+  
+
+
+  drawSprites();
+}
